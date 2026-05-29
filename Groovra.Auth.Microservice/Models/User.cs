bc4ecf74c777f80a62e6a4237cc1c667ed995746
Oleksander <sasaanickinanic@gmail.com>
2026-05-29 12:20:54 +0200

    Change Dataset<User>-> Unique name and IsPremium

--- Groovra.Auth.Microservice/Models/User.cs
+++ Groovra.Auth.Microservice/Models/User.cs
@@ -6,7 +6,8 @@ public class User
     public string Username { get; set; } = string.Empty;// можно сделать уникальным в будущем
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
-    public string Role { get; set; } = "Listener"; // Listener или Artist
+    public bool IsPremium { get; set; } = false;
+    public string Role { get; set; } = "Listener"; // Listener или Artist или Admin
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
     // Навигационное свойство — если юзер стал артистом
